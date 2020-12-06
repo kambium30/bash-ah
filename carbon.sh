7ee7095c76dbbad43a9eaa40f627afdbf9c42eef
A. Andriyadi <65210971+kambium30@users.noreply.github.com>
2020-12-06 20:11:11 +0700

    Update carbon.sh

--- carbon.sh
+++ carbon.sh
@@ -23,7 +23,7 @@ rm -rf prebuilts/clang/host/linux-x86/clang-12
 # Clone Tree
 git clone https://github.com/kambium30/android_device_xiaomi_santoni-1 -b cr-9.0 device/xiaomi/santoni
 git clone https://github.com/zhantech/android_kernel_xiaomi_santoni.git -b luuvy-4.9 kernel/xiaomi/msm8937
-git clone https://github.com/zhantech/vendor_xiaomi_santoni -b nad-11 vendor/xiaomi/
+git clone https://github.com/kambium30/vendor_xiaomi_santoni -b cr-8.0 vendor/xiaomi/
 
 # Clone Toolclain
 git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/clang-12
