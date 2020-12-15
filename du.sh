30f8c924bcbcb40485fd974518364c065dfcbefa
A. Andriyadi <65210971+kambium30@users.noreply.github.com>
2020-12-15 11:12:29 +0700

    Update and rename conquer.sh to du.sh

--- du.sh
+++ du.sh
@@ -1,9 +1,9 @@
 # Buka Directory
-cd ~/conquer
+cd ~/du
 
 # Sync Repo
-repo init -u git://github.com/ConquerOS/manifest.git -b ten
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
+repo init -u https://github.com/DirtyUnicorns/android_manifest.git -b q10x
+repo sync --force-sync -j$( nproc --all )
 
 # Hapus Out
 rm -rf out
@@ -12,15 +12,15 @@ rm -rf out/.lock
 # Hapus Tree
 rm -rf device/xiaomi/santoni
 rm -rf kernel/xiaomi/msm8937
-rm -rf vendor/xiaomi
+rm -rf vendor/xiaomi/santoni
 
 # Hapus Toolclain
 rm -rf prebuilts/clang/host/linux-x86/clang-12
 
 # Clone Tree
-git clone https://github.com/kambium30/device_xiaomi_santoni -b conquer device/xiaomi/santoni
+git clone https://github.com/kambium30/device_xiaomi_santoni -b du device/xiaomi/santoni
 git clone https://github.com/MrArtemSid/GameOver -b testing kernel/xiaomi/msm8937
-git clone https://github.com/kambium30/vendor_xiaomi_santoni -b cr-8.0 vendor/xiaomi/
+git clone https://github.com/kambium30/android_vendor_du -b q10x vendor/xiaomi/santoni
 
 # Clone Toolclain
 git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/clang-12
@@ -32,6 +32,6 @@ ccache -M 150G
 # Build Rom
 . build/envsetup.sh
 export KBUILD_BUILD_USER="kambium30"; export KBUILD_BUILD_HOST="ubuntu"
-lunch conquer_santoni-userdebug
-mka carthage -j12
+lunch santoni
+mka bacon
 
