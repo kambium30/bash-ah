d0ce59814f58040b2195212ed4ad379567f79c77
A. Andriyadi <65210971+kambium30@users.noreply.github.com>
2020-12-24 08:19:50 +0700

    Update and rename syberia.sh to arrow.sh

--- arrow.sh
+++ arrow.sh
@@ -1,9 +1,9 @@
 # Buka Directory
-cd ~/syberia
+cd ~/arrow
 
 # Sync Repo
-repo init -u https://github.com/syberia-project/manifest.git -b 10.0
-repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
+repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-10.0
+repo sync --force-sync --no-clone-bundle -j$(nproc --all)
 
 # Hapus Out
 rm -rf out
@@ -18,9 +18,9 @@ rm -rf vendor/xiaomi
 rm -rf prebuilts/clang/host/linux-x86/clang-12
 
 # Clone Tree
-git clone https://github.com/kambium30/device_xiaomi_santoni -b syberia device/xiaomi/santoni
-git clone https://github.com/MrArtemSid/GameOver -b testing kernel/xiaomi/msm8937
-git clone https://github.com/kambium30/vendor_xiaomi_santoni -b cr-8.0 vendor/xiaomi/
+git clone https://github.com/kambium30/device_xiaomi_santoni-2 -b arrow-ten device/xiaomi/santoni
+git clone https://github.com/zhantech/android_kernel_xiaomi_santoni -b luuvy-4.9 kernel/xiaomi/msm8937
+git clone https://github.com/kambium30/vendor_xiaomi -b arrow-ten vendor/xiaomi/
 
 # Clone Toolclain
 git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/clang-12
