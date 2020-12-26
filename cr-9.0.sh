376e04f52d745e3d190cfff5dde42b9bc3174f05
A. Andriyadi <65210971+kambium30@users.noreply.github.com>
2020-12-26 20:19:16 +0700

    Update cr-9.0.sh

--- cr-9.0.sh
+++ cr-9.0.sh
@@ -1,9 +1,9 @@
 # Buka Directory
-cd ~/aosip-11
+cd ~/cr-9.0
 
 # Sync Repo
-repo init -u git://github.com/AOSiP/platform_manifest.git -b eleven
-repo sync --force-sync --no-tags --no-clone-bundle
+repo init -u https://github.com/CarbonROM/android.git -b cr-9.0
+repo sync -ji
 
 # Hapus Out
 rm -rf out
@@ -21,9 +21,9 @@ rm -rf vendor/qcom/opensource/power
 rm -rf prebuilts/clang/host/linux-x86/clang-12
 
 # Clone Tree
-git clone https://github.com/kambium30/android_device_xiaomi_santoni -b aosip-11 device/xiaomi/santoni
+git clone https://github.com/kambium30/android_device_xiaomi_santoni-1 -b cr-9.0 device/xiaomi/santoni
 git clone https://github.com/zhantech/android_kernel_xiaomi_santoni -b luuvy-4.9 kernel/xiaomi/msm8937
-git clone https://github.com/zhantech/vendor_xiaomi_santoni -b nad-11 vendor/xiaomi/
+git clone https://github.com/kambium30/vendor_xiaomi_santoni -b cr-8.0-backup vendor/xiaomi/
 
 # Clone Toolclain
 git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/clang-12
@@ -37,8 +37,8 @@ rm -rf hardware/qcom-caf/wlan
 git clone https://github.com/SakilMondal/android_hardware_qcom-caf_wlan -b lineage-18.0 hardware/qcom-caf/wlan
 
 # Build Rom
-source build/envsetup.sh
+build/envsetup.sh
 export KBUILD_BUILD_USER="kambium30"; export KBUILD_BUILD_HOST="ruyo"
-lunch aosip_santoni-userdebug
-time m kronic
+lunch
+make carbon -j12
 
